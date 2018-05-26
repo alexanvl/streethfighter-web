7778cf3712bb5ef1bfdaec2c7de4cbda791a4f8f
James Young <james@james.local>
2018-05-26 02:30:45 -0300

    react sprites

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Spritesheet from 'react-responsive-spritesheet';
 import { injectRedux } from '../../components';
 import bgImage from './images/background.jpg';
 import meta from './meta';
 import styles from './styles';
 
-export default injectRedux(props => {
-  const { classes, firebaseActions, history, location } = props;
-  const about = [];
-  //firebaseActions.listenOn('test', console.log)
-  return (
-    <div>
-      Hello World
-    </div>
-  );
-});
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div className="rrs-container">
+        <Spritesheet
+          className={`my-element__class--style`}
+          image={`https://raw.githubusercontent.com/danilosetra/react-responsive-spritesheet/master/assets/images/examples/sprite-image-horizontal.png`}
+          widthFrame={420}
+          heightFrame={500}
+          steps={14}
+          fps={10}
+          autoplay={true}
+          loop={true}
+        />
+      </div>
+    );
+  }
+}
+
+export default injectRedux(Home);
+
