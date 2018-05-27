5ecf7b8c55c7293020d2959421de73650b66a024
Aidan Musnitzky <musnit@gmail.com>
2018-05-27 04:01:16 -0300

    fix home

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -6,7 +6,7 @@ import bgImage from './images/background.jpg';
 import meta from './meta';
 import styles from './styles';
 // import signup from './components/Signup';
-import Fight from './components/Fight'
+
 class Home extends Component {
   constructor() {
     super();
@@ -17,21 +17,20 @@ class Home extends Component {
 
   render() {
     return (
-        <Spritesheet
-          image={`https://raw.githubusercontent.com/danilosetra/react-responsive-spritesheet/master/assets/images/examples/sprite-image-horizontal.png`}
-          widthFrame={420}
-          heightFrame={500}
-          steps={14}
-          fps={10}
-          autoplay={true}
-          loop={true}
-        />
-        // <div>
-
-        //   //<Layer2LibTester />
-        // </div>
+        // <Spritesheet
+        //   image={`https://raw.githubusercontent.com/danilosetra/react-responsive-spritesheet/master/assets/images/examples/sprite-image-horizontal.png`}
+        //   widthFrame={420}
+        //   heightFrame={500}
+        //   steps={14}
+        //   fps={10}
+        //   autoplay={true}
+        //   loop={true}
+        // />
+        <div>
+          <Layer2LibTester />
+        </div>
     );
   }
 }
 
-export default injectRedux(Fight);
+export default injectRedux(Home);
