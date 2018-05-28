94458335a0f622484ba6dcb35bb3257e6e44005a
Lex Lewthor <alexvlewis@gmail.com>
2018-05-28 10:38:28 -0700

    Migrated api to https://github.com/alexanvl/streethfighter-api.git

--- api/src/helpers/error.js
+++
@@ -1,9 +0,0 @@
-function APIError(code, message) {
-  this.statusCode = code;
-  this.message = message;
-  this.stack = (new Error()).stack;
-}
-
-APIError.prototype = new Error();
-
-module.exports = APIError;
